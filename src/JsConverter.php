bfcb6c3bc2d4b934b410f52c50dfd4536313012c
Bradie Tilley <44430471+bradietilley@users.noreply.github.com>
2022-03-04 21:16:53 +0800

    Remove functions to ensure more sources of JavaScript objects can be supported

--- src/JsConverter.php
+++ src/JsConverter.php
@@ -22,21 +22,24 @@ class JsConverter
     {
         $replacedStringsList = [];
 
+        // 0. Remove functions from objects
+        $convertedString = static::removeFunctions($jsObjectString);
+
         // 1. Replace all delimited string literals with placeholders
-        $convertedString = self::replaceSectionsWithPlaceholders($jsObjectString, $replacedStringsList, "'");
+        $convertedString = self::replaceSectionsWithPlaceholders($convertedString, $replacedStringsList, "'");
         self::fixQuoteEscapingForSingleQuoteDelimitedStrings($replacedStringsList);
         $convertedString = self::replaceSectionsWithPlaceholders($convertedString, $replacedStringsList, '"');
 
         // 2. Now is safe to remove all white space
         $convertedString = preg_replace('/\s+/m', '', $convertedString);
 
-        // 2b. And remove all trailing commas in objects
+        // 3. And remove all trailing commas in objects
         $convertedString = str_replace(',}', '}', $convertedString);
 
-        // 3. Add double quotes for keys
+        // 4. Add double quotes for keys
         $convertedString = preg_replace('/([^{}\[\]#,]+):/', '"$1":', $convertedString);
 
-        // 4. Add double quotes for values
+        // 5. Add double quotes for values
         $convertedString = preg_replace_callback(
             '/:([^{}\[\]#,]+)/',
             function ($matches) {
@@ -49,12 +52,12 @@ class JsConverter
             $convertedString
         );
 
-        // 5. Make sure "true", "false" and "null" values get delimited by double quotes
+        // 6. Make sure "true", "false" and "null" values get delimited by double quotes
         // Need to run the replacement twice, because not all values get replaced if they are adjacent
         $convertedString = preg_replace('/([^"])(true|false|null)([^"])/', '$1"$2"$3', $convertedString);
         $convertedString = preg_replace('/([^"])(true|false|null)([^"])/', '$1"$2"$3', $convertedString);
 
-        // 6. Replace the placeholders with the initial strings
+        // 7. Replace the placeholders with the initial strings
         $deep = false;
         do {
             $convertedString = preg_replace_callback(
@@ -68,6 +71,133 @@ class JsConverter
             );
             $deep = true;
         } while (!empty($replacedStringsList));
+
+        return $convertedString;
+    }
+
+    public static function convertToArray(string $input): ?array
+    {
+        $json = static::convertToJson($input);
+
+        return json_decode($json, true);
+    }
+
+    /**
+     * Remove all functions (bar variable based functions) from the JavaScript object.
+     * 
+     * Removes shorthand and longhand functions whether they're single or multi-line:
+     *     key: (var) => 'Test',
+     *     key: var => 'Test',
+     *     key: () => 'Test',
+     *     key: () => { return 'Test'; },
+     *     key: (var) => {
+     *         return 'Test';
+     *     },
+     *     key: () => {
+     *         return 'Test';
+     *     },
+     *     key: () => {
+     *         if (complex) {
+     *             return 'Test';
+     *         }
+     * 
+     *         return 'Test';
+     *     },
+     * 
+     * To do this, it will first look for any row tat matches a function opening (e.g. '(*) =>' or 'function (*)')
+     * then will keep deleting subsequent rows until it finds a close function tag ('}') that matches the indentation
+     * of the opening function (where number of spaces/tabs match) or the parent object closes its object ('}').
+     * 
+     * This function won't modify any JavaScript objects unless a function is identified.
+     *
+     * @param string $input
+     * @param boolean $debug
+     * @return void
+     */
+    public static function removeFunctions(string $input, bool $debug = false)
+    {
+        $functionLines = '/^(\s*)([\'"]?\w+[\'"]?):\s*((?:function\s*)\([^\)]*\)\s*{|\s*(?:\([^)]*\)|[a-z0-9]+)\s*=>\s*)/';
+        $lines = preg_split('/[\n\r]/', $input);
+
+        $deleteUntil = null;
+        $deleteUntilSpaces = null;
+        $table = [];
+
+        foreach ($lines as $index => $line) {
+            $row = [
+                'line' => $line,
+                'mode' => 'standard',
+                'action' => 'Keeping',
+            ];
+
+            if (preg_match($functionLines, $line, $m)) {
+                $deleteUntil = '/^(' . $m[1] . ')([a-z\'"}])/';
+                unset($lines[$index]);
+
+                $deleteUntilSpaces = strlen($m[1]);
+
+                $row['mode'] = 'Start (spaces: ' . $deleteUntilSpaces . ')';
+                $row['action'] = 'Delete';
+                $table[] = $row;
+                continue;
+            }
+
+            if ($deleteUntil !== null) {
+                $row['mode'] = 'DU: ';
+
+                if (preg_match($deleteUntil, $line, $m)) {
+                    $count = strlen($m[1]);
+                    $deleteIf = ($m[2]) === '}';
+
+                    $row['mode'] .= 'End (of function) (spaces: ' . $count . ')';
+                    
+                    if ($deleteIf) {
+                        unset($lines[$index]);
+                        $row['action'] = 'Delete';
+                    } else {
+                        $row['action'] = 'Keeping';
+                        $deleteUntil = null;
+                    }
+                } elseif (preg_match('/^(\s*)}/', $line, $m)) {
+                    $spaces = strlen($m[1]);
+
+                    if ($spaces < $deleteUntilSpaces) {
+                        // Still looping through function lines
+                        $row['mode'] .= 'End (of object) (spaces: ' . $spaces . ')';
+                        $row['action'] = 'Keeping';
+                        $deleteUntil = null;
+                    } else {
+                        // Still looping through function lines
+                        $row['mode'] .= 'Middle of function (false ending)';
+                        unset($lines[$index]);
+                        $row['action'] = 'Delete';
+                    }
+                } else {
+                    // Still looping through function lines
+                    $row['mode'] .= 'Middle of function';
+                    unset($lines[$index]);
+                    $row['action'] = 'Delete';
+                }
+            } else {
+                $row['action'] = 'Keeping';
+            }
+
+            if ($debug) {
+                $table[] = $row;
+            }
+        }
+
+        if ($debug) {
+            print '<table><tr><th>Line</th><th>Mode</th><th>Action</th></tr>';
+            foreach ($table as $row) {
+                print '<tr><td><pre>'. $row['line'] . '</pre></td><td>'. $row['mode'] . '</td><td>'. $row['action'] . '</td></tr>';
+            }
+            print '</table>';
+
+            die();
+        }
+
+        $convertedString = implode("\n", $lines);
 
         return $convertedString;
     }
